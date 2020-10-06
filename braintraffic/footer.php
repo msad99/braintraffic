f790ee23672e1d81a4ab01c0a98585469c569d8c
msad99 <43373728+msad99@users.noreply.github.com>
2020-10-06 15:08:15 +0200

    Hele tema mappen
    Tema mappe upload

---
+++ braintraffic/footer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package BrainTraffic
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<ul class="footer-list" role="list">
+		<li class="list-item-footer">
+			<div class="footer-link">© 2020 Brain Traffic</div>
+		</li>
+		<li class="list-item-footer">
+			<a href="http://confabevents.com" class="footer-link">Confab Conference</a>
+		</li>
+		<li class="list-item-footer">
+			<a href="http://buttonconf.com" class="footer-link">Button Conference</a>
+		</li><li class="list-item-footer">
+			<a href="https://www.contentstrategy.com/podcast" class="footer-link">Content Strategy Podcast</a>
+		</li>
+		<li class="list-item-footer">
+			<a href="/contact" class="footer-link">Contact Us</a>
+		</li>
+		<li class="list-item-footer">
+			<a href="/mailing-list" class="footer-link">Newsletter</a>
+		</li>
+		<li class="list-item-footer">
+			<a href="http://twitter.com/braintraffic" class="footer-link">Twitter</a>
+		</li>
+		<li class="list-item-footer last">
+			<a href="https://www.linkedin.com/company/brain-traffic" class="footer-link">LinkedIn</a>
+		</li>
+			</ul>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
